e56d2667802fee9cfaa2556e2e53c0c999d93067
Zain Adam <zainadam.id@gmail.com>
2024-06-23 04:33:41 +0800

    chore: add new components

---
+++ resources/views/components/references-section.blade.php
@@ -0,0 +1,17 @@
+<section>
+    <x-h2>
+        References
+    </x-h2>
+
+    <ul>
+        @foreach ($links as $link)
+        <li>
+            <a href="{{ $link }}" target="_blank">
+                <x-mbc::typography variant="body2">
+                    {{ $link }} <x-mbc::icon style="font-size: inherit;" icon="open_in_new" />
+                </x-mbc::typography>
+            </a>
+        </li>
+        @endforeach
+    </ul>
+</section>
