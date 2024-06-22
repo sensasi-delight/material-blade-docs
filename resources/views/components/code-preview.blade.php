2c5424cdd9d7843f6e24b359694fd69c5024c1eb
Zain Adam <zainadam.id@gmail.com>
2024-06-23 03:23:42 +0800

    chore: `$code` slot can be `null`

--- resources/views/components/code-preview.blade.php
+++ resources/views/components/code-preview.blade.php
@@ -1,13 +1,18 @@
-<x-mbc::card variant="outlined" style="background-color: #FAFAFA; padding: 32px; display: flex; align-items: center; overflow: auto;">
-    {{ $slot }}
-</x-mbc::card>
+<div>
+    <x-mbc::card variant="outlined" style="background-color: #FAFAFA; padding: 32px; display: flex; align-items: center; overflow: auto;">
+        {{ $slot }}
+    </x-mbc::card>
 
-<?php $id = uniqid(); ?>
+    @isset($code)
 
-<x-mbc::button variant="outlined" fullwidth onclick="
+    <?php $id = uniqid(); ?>
+
+    <x-mbc::button variant="outlined" fullwidth onclick="
     document.getElementById('{{ $id }}').classList.toggle('show');
-">
-    Show code
-</x-mbc::button>
+    ">
+        Show code
+    </x-mbc::button>
 
-<pre id="{{ $id }}" class="src-code"><code class="language-php">{{ $code }}</code></pre>
+    <pre id="{{ $id }}" class="src-code"><code class="language-php">{{ $code }}</code></pre>
+    @endisset
+</div>
