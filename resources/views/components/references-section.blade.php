55c4d4c40c18c37ebc90b9d88f2b09ef03cd0ae6
Zain Adam <zainadam.id@gmail.com>
2024-06-27 05:02:19 +0800

    chore: disable item margin

--- resources/views/components/references-section.blade.php
+++ resources/views/components/references-section.blade.php
@@ -5,13 +5,13 @@
 
     <ul>
         @foreach ($links as $link)
-        <li>
-            <a href="{{ $link }}" target="_blank">
-                <x-mbc::typography variant="body2">
-                    {{ $link }} <x-mbc::icon style="font-size: inherit;" icon="open_in_new" />
-                </x-mbc::typography>
-            </a>
-        </li>
+            <li>
+                <a href="{{ $link }}" target="_blank">
+                    <x-mbc::typography class="mbc-m-0" variant="body2">
+                        {{ $link }} <x-mbc::icon style="font-size: inherit;" icon="open_in_new" />
+                    </x-mbc::typography>
+                </a>
+            </li>
         @endforeach
     </ul>
 </section>
