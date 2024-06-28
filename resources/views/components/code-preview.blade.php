1147b3c3b7e3044e6f1f6845e43c03490a9e51c8
Zain Adam <zainadam.id@gmail.com>
2024-06-28 22:12:33 +0800

    chore: improve code display

--- resources/views/components/code-preview.blade.php
+++ resources/views/components/code-preview.blade.php
@@ -1,27 +1,67 @@
-<div>
+{{-- TODO: rename to component preview --}}
+<div class="mbc-mx-8">
     <x-mbc::card
         variant="outlined"
-        style="background-color: #FAFAFA; padding: 32px; display: flex; align-items: center; overflow: auto;"
+        class="mbc-p-2"
+        style="background-color: #FAFAFA"
     >
         {{ $slot }}
     </x-mbc::card>
 
-    @isset($code)
-        <?php $id = uniqid(); ?>
+    @if (isset($code) || isset($codeSummary))
+        @php
+            $id = uniqid();
+        @endphp
+    @endif
 
+    @isset($code)
         <x-mbc::button
-            variant="outlined"
-            fullwidth
             onclick="
-    document.getElementById('{{ $id }}').classList.toggle('show');
-    "
+            switch (this.innerText) {
+                case 'SHOW CODE':
+                    this.innerText = 'HIDE CODE';
+                    break;
+
+                case 'HIDE CODE':
+                    this.innerText = 'SHOW CODE';
+                    break;
+
+                case 'EXPAND CODE':
+                    this.innerText = 'COLLAPSE CODE';
+                    break;
+
+                case 'COLLAPSE CODE':
+                    this.innerText = 'EXPAND CODE';
+                    break;
+            
+                default:
+                    throw new Error('Invalid button text');
+                    break;
+            }
+            document.getElementById('{{ $id }}').classList.toggle('show')
+            document.getElementById('{{ $id }}-summary').classList.toggle('show')
+        "
         >
-            Show code
+            @if (isset($codeSummary))
+                EXPAND CODE
+            @else
+                SHOW CODE
+            @endif
         </x-mbc::button>
 
         <pre
             id="{{ $id }}"
             class="src-code"
+            style="border-radius: var(--mdc-shape-medium, 4px);"
+            @class(['show' => true])
         ><code class="language-php">{{ $code }}</code></pre>
+    @endisset
+
+    @isset($codeSummary)
+        <pre
+            id="{{ $id }}-summary"
+            style="border-radius: var(--mdc-shape-medium, 4px);"
+            @class(['src-code', 'show' => true])
+        ><code class="language-php">{{ $codeSummary }}</code></pre>
     @endisset
 </div>
