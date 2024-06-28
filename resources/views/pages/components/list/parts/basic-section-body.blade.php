3016a6dd9cd3aa54f9b5a6ab32c7a1d2726280e4
Zain Adam <zainadam.id@gmail.com>
2024-06-28 22:25:32 +0800

    refactor: change single line section to basic section

---
+++ resources/views/pages/components/list/parts/basic-section-body.blade.php
@@ -0,0 +1,50 @@
+<x-mbc::typography>
+    <code>List</code> component is a wrapper component that contains <code>ListItem</code> and
+    <code>ListDivider</code> components.
+</x-mbc::typography>
+
+<x-mbc::typography>
+    The basic structure of a list is as follows:
+</x-mbc::typography>
+
+<x-component-preview>
+    <x-mbc::list>
+        <x-mbc::list-item>
+            Item 1
+        </x-mbc::list-item>
+
+        <x-mbc::list-item>
+            Item 2
+        </x-mbc::list-item>
+
+        <x-mbc::list-item>
+            Item 3
+        </x-mbc::list-item>
+    </x-mbc::list>
+
+    {{-- prettier-ignore-start --}}
+    <x-slot:code-summary>&lt;x-mbc::list>
+    &lt;x-mbc::list-item>
+        Item 1
+    &lt;/x-mbc::list-item>
+
+    // Other list items
+    // ....
+&lt;/x-mbc::list></x-slot:code-summary>
+
+    <x-slot:code>&lt;x-mbc::list>
+    &lt;x-mbc::list-item>
+        Item 1
+    &lt;/x-mbc::list-item>
+
+    &lt;x-mbc::list-item>
+        Item 2
+    &lt;/x-mbc::list-item>
+
+    &lt;x-mbc::list-item>
+        Item 3
+    &lt;/x-mbc::list-item>
+&lt;/x-mbc::list></x-slot:code>
+        {{-- prettier-ignore-end --}}
+
+</x-component-preview>
