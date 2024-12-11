c6af8390aa0fc45e6f24ce8d571187e39d7c4e7d
Zain🍕 <zainadam.id@gmail.com>
2024-12-12 02:55:40 +0800

    feat: update DocRoute enum with new component cases and rename existing case

--- app/Enums/DocRoute.php
+++ app/Enums/DocRoute.php
@@ -4,10 +4,28 @@ namespace App\Enums;
 
 enum DocRoute: string
 {
-    case GETTINGSTARTED = 'getting-started';
+    case GETTING_STARTED = 'getting-started';
+    // case COMPONENTS_APP_BAR = 'components.app-bar';
+    case COMPONENTS_BANNER = 'components.banner';
     case COMPONENTS_BUTTON = 'components.button';
+    // case COMPONENTS_CARD = 'components.card';
+    // case COMPONENTS_CHIP = 'components.chip';
+    // case COMPONENTS_DATA_TABLE = 'components.data-table';
+    // case COMPONENTS_DIALOG = 'components.dialog';
     case COMPONENTS_FAB = 'components.fab';
     case COMPONENTS_ICON = 'components.icon';
+    // case COMPONENTS_IMAGE_LIST = 'components.image-list';
     case COMPONENTS_ICON_BUTTON = 'components.icon-button';
     case COMPONENTS_LIST = 'components.list';
+    // case COMPONENTS_MENU = 'components.menu';
+    // case COMPONENTS_NAVIGATION_DRAWER = 'components.navigation-drawer';
+    // case COMPONENTS_PROGRESS_INDICATOR = 'components.progress-indicator';
+    // case COMPONENTS_RADIO_BUTTON = 'components.radio-button';
+    // case COMPONENTS_SLIDER = 'components.slider';
+    // case COMPONENTS_SNACKBAR = 'components.snackbar';
+    // case COMPONENTS_SWITCH = 'components.switch';
+    // case COMPONENTS_TAB_BAR = 'components.tab-bar';
+    // case COMPONENTS_TEXT_FIELD = 'components.text-field';
+    // case COMPONENTS_TOOLTIP = 'components.tooltip';
+    // case COMPONENTS_TYPOGRAPHY = 'components.typography';
 }
